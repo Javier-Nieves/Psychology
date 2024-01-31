aa2694dc75422cd416dbc4437d7d49712bbb4c85
Javier Nieves <javier.nikolskiy@gmail.com>
2024-01-31 20:39:19 +0100

    more sections

--- psychology.js
+++ psychology.js
@@ -1,1 +1,17 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const animationPath = "/files/therapy.json";
+
+  // Replace 'lottie-container' with the ID of your container element
+  const container = document.getElementById("lottie-container");
+  console.log("selected", container);
+
+  // Load the Lottie animation
+  const animation = lottie.loadAnimation({
+    container: container,
+    renderer: "svg", // You can use 'canvas' or 'html' as well
+    loop: true,
+    autoplay: true,
+    path: animationPath,
+  });
+});
